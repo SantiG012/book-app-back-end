0e8670fd2208ea6b9b2186199e27042eff36a9c0
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-15 14:54:04 -0500

    Add the book module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, BookModule],
   controllers: [],
   providers: []
 })
