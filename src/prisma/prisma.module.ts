ae0c5c182263dbe5fddab6b8cf32862302c25b85
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-16 11:52:53 -0500

    Add the prisma service to the prisma module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma-service/prisma.service';
+import { AbstractPrismaService } from './abstract-prisma.service';
 
-@Module({})
+@Module({
+  providers: [{
+    provide: AbstractPrismaService,
+    useClass: PrismaService
+  }]
+})
 export class PrismaModule {}
