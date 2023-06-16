983b1dceed22e7aaf8baaf8311fd3ec21454da84
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-16 11:49:15 -0500

    Create the abstract prisma service

--- src/prisma/abstract-prisma.service.ts
+++ src/prisma/abstract-prisma.service.ts
@@ -1,3 +1,6 @@
-export abstract class AbstractPrismaService {
-    abstract cleanDb():void;
+import { PrismaClient } from '@prisma/client';
+
+export abstract class AbstractPrismaService extends PrismaClient {
+    //TODO: set the return type
+  abstract cleanDb();
 }
