c9cec86eb6b718c6e0716f61f6328fb428521414
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-17 13:22:33 -0500

    Add the create book method

--- src/book/book-controller/book.controller.ts
+++ src/book/book-controller/book.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller,Post} from '@nestjs/common';
+import { AbstracBookService } from '../abstract-book.service';
+import { BookCreationDto } from '../book-dtos';
+import { Book } from '@prisma/client';
+import { Observable, catchError, tap } from 'rxjs';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+    constructor(private readonly bookService: AbstracBookService) {}
+    @Post('createBook')
+    createBook(@Body() bookDto:BookCreationDto):Observable<Book> {
+        
+        return this.bookService.createBook(bookDto).pipe(
+            tap((book:Book) => console.log(book)),
+            catchError((err) => {throw new Error(err)})
+        )
+    }
+}
