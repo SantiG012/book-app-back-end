11ec659bcbde62fd9fcfca31b139f70df620f361
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-17 15:50:05 -0500

    Move the error handling logic to the service layer

--- src/book/book-controller/book.controller.ts
+++ src/book/book-controller/book.controller.ts
@@ -2,17 +2,15 @@ import { Body, Controller,Post} from '@nestjs/common';
 import { AbstracBookService } from '../abstract-book.service';
 import { BookCreationDto } from '../book-dtos';
 import { Book } from '@prisma/client';
-import { Observable, catchError, tap } from 'rxjs';
+import { Observable} from 'rxjs';
 
 @Controller('book')
 export class BookController {
     constructor(private readonly bookService: AbstracBookService) {}
+
     @Post('createBook')
-    createBook(@Body() bookDto:BookCreationDto):Observable<Book> {
+    createBook(@Body() bookDto:BookCreationDto):Observable<{bookId:string}> {
         
-        return this.bookService.createBook(bookDto).pipe(
-            tap((book:Book) => console.log(book)),
-            catchError((err) => {throw new Error(err)})
-        )
+        return this.bookService.createBook(bookDto);
     }
 }
--- src/book/book-service/book.service.ts
+++ src/book/book-service/book.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { AbstracBookService } from '../abstract-book.service';
 import { AbstractPrismaService } from 'src/prisma/abstract-prisma.service';
 import { BookCreationDto } from '../book-dtos';
 import { v4 as uuidv4 } from "uuid";
-import { Observable, from } from 'rxjs';
+import { Observable, catchError, from, throwError } from 'rxjs';
 import { Book } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class BookService implements AbstracBookService {
     constructor(private readonly prisma: AbstractPrismaService) {}
     
-    createBook(book:BookCreationDto):Observable<Book> {
+    createBook(book:BookCreationDto):Observable<{bookId:string}> {
         const data = {
             bookId: uuidv4(),
             bookTitle: book.title,
@@ -18,6 +19,22 @@ export class BookService implements AbstracBookService {
             bookStatus:'active'
         };
 
-        return from(this.prisma.book.create({data}));
+        return from(this.prisma.book.create({
+            data:data,
+            select:{
+                bookId:true
+            }
+        })).pipe(
+            catchError((error:PrismaClientKnownRequestError)=>{
+                return throwError(()=>this.createError(error.code))
+            })
+        );
+    }
+
+    private createError(erroCode:string):HttpException{
+        if (erroCode === 'P2002') {
+            return new HttpException('Book already exists', 409);
+        }
+        return new HttpException('Internal server error', 500)
     }
 }
