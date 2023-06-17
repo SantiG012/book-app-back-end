ceb6990cb019d8c72f54806287de470075950f92
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-17 13:21:51 -0500

    Create the book DTO

---
+++ src/book/book-dtos/bookCreationDto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsString, ValidateIf } from "class-validator";
+
+export class BookCreationDto {
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+
+    @ValidateIf((o) => {return ((o.coverUrl !== undefined) || (o.coverUrl !== null))})
+    @IsString()
+    coverUrl: string | null;
+}
