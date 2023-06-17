76b95e90a2079282cfab0aaabc2a035b0b5174e5
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-17 13:22:12 -0500

    Import the config module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,12 +1,15 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma-service/prisma.service';
 import { AbstractPrismaService } from './abstract-prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
+  imports: [ConfigModule],
   providers: [{
     provide: AbstractPrismaService,
     useClass: PrismaService
-  }]
+  }],
+  exports: [AbstractPrismaService]
 })
 export class PrismaModule {}
