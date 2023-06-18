95ad73e0c9d3a97ecf4b7d45bf41820f9b91a9ca
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-18 14:14:37 -0500

    Creates the controller instance test

--- src/book/test/book.controller.spec.ts
+++ src/book/test/book.controller.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookController } from '../book.controller';
+import { BookService } from '../book.service';
+import { AbstracBookService } from '../abstract-book.service';
+
+jest.mock('../book.service');
 
 describe('BookController', () => {
-  let controller: BookController;
+  let bookController: BookController;
+  let bookService: AbstracBookService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BookController],
+      providers: [{
+        provide: AbstracBookService,
+        useClass: BookService
+      }]
     }).compile();
 
-    controller = module.get<BookController>(BookController);
+    bookController = module.get<BookController>(BookController);
+    bookService = module.get<AbstracBookService>(AbstracBookService);
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(bookController).toBeDefined();
   });
+
 });
