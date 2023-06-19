ef5def7697f5d7da3b710380b5cc2923b1ba5f37
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-19 10:42:16 -0500

    Remove unused imports

--- src/book/abstract-book.service.ts
+++ src/book/abstract-book.service.ts
@@ -1,5 +1,5 @@
 import { BookCreationDto,BookCreationResponseDto } from "./book-dtos";
-import { Book } from "@prisma/client";
+
 
 export abstract class AbstracBookService{
     abstract  createBook(book: BookCreationDto):Promise<BookCreationResponseDto>;
