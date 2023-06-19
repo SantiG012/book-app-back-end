3c05fd83c300bc29ee56a62b8d175a33201b9283
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-19 10:40:09 -0500

    Remove the observables operators

--- src/book/__mocks__/book.service.ts
+++ src/book/__mocks__/book.service.ts
@@ -1,6 +1,5 @@
 import {bookStub} from '../test/stubs/book.stub';
-import { of} from 'rxjs';
 
 export const BookService = jest.fn().mockReturnValue({
-    createBook: jest.fn().mockResolvedValue(of(bookStub()))
+    createBook: jest.fn().mockResolvedValue(bookStub())
 })
