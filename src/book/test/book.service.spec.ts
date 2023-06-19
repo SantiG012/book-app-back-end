6e709d5f612e109281c33a3b483f4cfb982fe8df
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-19 11:10:59 -0500

    Add the AbstractBookService to the BookService

--- src/book/test/book.service.spec.ts
+++ src/book/test/book.service.spec.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookService } from '../book.service';
+import { AbstracBookService } from '../abstract-book.service';
 
 describe('BookService', () => {
-  let service: BookService;
+  let service: AbstracBookService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookService],
+      providers: [{
+        provide: AbstracBookService,
+        useClass: BookService
+      }],
     }).compile();
 
-    service = module.get<BookService>(BookService);
+    service = module.get<AbstracBookService>(AbstracBookService);
   });
 
   it('should be defined', () => {
