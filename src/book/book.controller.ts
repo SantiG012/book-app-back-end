01d9a2990903c090d9e061ec55f0a3f13e2f5193
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-19 10:09:30 -0500

    Change the observable to promise

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,15 +1,13 @@
 import { Body, Controller,Post} from '@nestjs/common';
 import { AbstracBookService } from './abstract-book.service';
-import { BookCreationDto } from './book-dtos';
-import { Book } from '@prisma/client';
-import { Observable} from 'rxjs';
+import { BookCreationDto, BookCreationResponseDto } from './book-dtos';
 
 @Controller('book')
 export class BookController {
     constructor(private readonly bookService: AbstracBookService) {}
 
     @Post('createBook')
-    createBook(@Body() bookDto:BookCreationDto):Observable<{bookId:string}> {
+    createBook(@Body() bookDto:BookCreationDto):Promise<BookCreationResponseDto> {
         
         return this.bookService.createBook(bookDto);
     }
