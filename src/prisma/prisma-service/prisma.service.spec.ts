19b0a90ce12a4cc1b46bab90562bb9b9ceacfb44
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-19 11:28:28 -0500

    Add the prisma service test

--- src/prisma/prisma-service/prisma.service.spec.ts
+++ src/prisma/prisma-service/prisma.service.spec.ts
@@ -1,15 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from './prisma.service';
+import { AbstractPrismaService } from '../abstract-prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('PrismaService', () => {
-  let service: PrismaService;
+  let service: AbstractPrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PrismaService],
+      imports: [ConfigModule],
+      providers: [{
+        provide: AbstractPrismaService,
+        useClass: PrismaService
+      }],
     }).compile();
 
-    service = module.get<PrismaService>(PrismaService);
+    service = module.get<AbstractPrismaService>(AbstractPrismaService);
   });
 
   it('should be defined', () => {
