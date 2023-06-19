d7a12ec8b06d0621b4dd6cb3e3a52a408bf6b10c
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-19 14:06:20 -0500

    Add the prisma service test

--- src/prisma/test/prisma.service.spec.ts
+++ src/prisma/test/prisma.service.spec.ts
@@ -2,6 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../prisma.service';
 import { AbstractPrismaService } from '../abstract-prisma.service';
 import { ConfigModule } from '@nestjs/config';
+import { BookCreationResponseDto } from 'src/book/book-dtos';
+import { Book } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { HttpException } from '@nestjs/common';
+
+jest.mock('../prisma.service');
+
 
 describe('PrismaService', () => {
   let service: AbstractPrismaService;
@@ -21,4 +28,69 @@ describe('PrismaService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('create a new book',()=>{
+    describe('when create a new book',()=>{
+      let bookId:BookCreationResponseDto;
+
+      const book:Book= {
+        bookId:'bookId',
+        bookTitle:'1984',
+        coverUrl:null,
+        bookStatus:'active'
+      }
+
+      beforeEach(async()=>{
+
+        bookId = await service.book.create({
+          data:book
+        });
+      });
+
+      test('then it should call prisma.book.create',()=>{
+        expect(service.book.create).toBeCalledWith({
+          data:book
+        });
+      });
+
+      test('then it should return a bookId',()=>{
+        expect(bookId).toEqual({bookId:book.bookId});
+      });
+    });
+  });
+
+  describe('create an existing book',()=>{
+    describe('when create an existing book',()=>{
+      let bookId:BookCreationResponseDto;
+
+      const book:Book= {
+        bookId:'bookId',
+        bookTitle:'1984',
+        coverUrl:null,
+        bookStatus:'active'
+      }
+
+      beforeEach(async()=>{
+        service.book.create = jest.fn().mockRejectedValue({
+          code:'P2002'
+        });
+
+        try{
+          bookId = await service.book.create({
+            data:book
+          });
+        }catch(e:PrismaClientKnownRequestError | any){
+
+        }
+
+      });
+
+      test('then it should throw an error',()=>{
+        expect(()=>{
+          throw new HttpException('Book already exists', 409);
+        }).toThrow();
+      });
+    });
+  });
+
 });
