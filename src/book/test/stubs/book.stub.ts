8985644d8bb3be2e4d43f307aee98afd8224e385
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-19 10:40:47 -0500

    Type the bockStub

--- src/book/test/stubs/book.stub.ts
+++ src/book/test/stubs/book.stub.ts
@@ -1,5 +1,9 @@
-export const bookStub = ():{bookId:string} => {
-    return {
-        bookId: 'bookId',
-    };
+import { BookCreationResponseDto } from "src/book/book-dtos";
+
+export const bookStub = ():BookCreationResponseDto => {
+    const bookId:BookCreationResponseDto = {
+        bookId:'bookId'
+    }
+
+    return bookId;
 }
