fe1cea5a4ebfa96d5bbf76f668d17ea2f08cbedf
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-01 11:43:38 -0500

    Delete unused files

--- src/prisma/test/stubs/book.stub.ts
+++
@@ -1,9 +0,0 @@
-import { BookCreationResponseDto } from "src/book/book-dtos";
-
-export const bookStub = ():BookCreationResponseDto => {
-    const bookId:BookCreationResponseDto = {
-        bookId:'bookId'
-    }
-
-    return bookId;
-}
