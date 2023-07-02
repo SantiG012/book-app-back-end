879e6732ed94ff9c311c6ce9dd75649645d2cb51
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-02 12:28:23 -0500

    Add the post route

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateAuthorDto } from './author-dtos';
+import { AbstractAuthorService } from './abstract-author.service';
 
 @Controller('author')
-export class AuthorController {}
+export class AuthorController {
+    constructor(
+        private readonly authorService: AbstractAuthorService
+    ) {}
+
+    @Post('createAuthor')
+    async createAuthor(@Body() createAuthorDto: CreateAuthorDto) {
+        return await this.authorService.createAuthor(createAuthorDto);
+    }
+}
