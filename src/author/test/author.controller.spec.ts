3f6e15abfc4501107ee4214b53e02b73d245ba8f
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-02 12:45:47 -0500

    Mock the author service in the test file

--- src/author/test/author.controller.spec.ts
+++ src/author/test/author.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorController } from '../author.controller';
+import { AbstractAuthorService } from '../abstract-author.service';
+import { AuthorService } from '../author.service';
+
+jest.mock('../author.service');
 
 describe('AuthorController', () => {
   let controller: AuthorController;
@@ -7,6 +11,10 @@ describe('AuthorController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthorController],
+      providers: [{
+        provide: AbstractAuthorService,
+        useClass: AuthorService
+      }]
     }).compile();
 
     controller = module.get<AuthorController>(AuthorController);
