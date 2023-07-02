5635d53d332b066251705083822bcdc64adab062
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-02 12:01:21 -0500

    Add the author id DTO

---
+++ src/author/author-dtos/author-id.dto.ts
@@ -0,0 +1,3 @@
+export interface AuthorIdDto {
+    authorId: string;
+}
--- src/author/author-dtos/index.ts
+++ src/author/author-dtos/index.ts
@@ -1,3 +1,5 @@
 import { CreateAuthorDto } from "./create-author.dto";
+import { AuthorIdDto } from "./author-id.dto";
 
 export * from "./create-author.dto";
+export * from "./author-id.dto";
