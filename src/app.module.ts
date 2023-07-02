61708a96b8fad3d0f4203b50eb8babf97aca5feb
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-02 09:45:09 -0500

    Create the author module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BookModule } from './book/book.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthorModule } from './author/author.module';
 
 @Module({
-  imports: [AuthModule, BookModule, PrismaModule],
+  imports: [AuthModule, BookModule, PrismaModule, AuthorModule],
   controllers: [],
   providers: []
 })
