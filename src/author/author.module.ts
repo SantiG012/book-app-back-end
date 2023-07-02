10ff781f9f0af5bf2e390c7dd2dba6c049a47bda
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-02 12:27:42 -0500

    Add the abstract class to the providers

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthorController } from './author.controller';
 import { AuthorService } from './author.service';
+import { AbstractAuthorService } from './abstract-author.service';
 
 @Module({
   controllers: [AuthorController],
-  providers: [AuthorService]
+  providers: [{
+    provide: AbstractAuthorService,
+    useClass: AuthorService
+  }]
 })
 export class AuthorModule {}
