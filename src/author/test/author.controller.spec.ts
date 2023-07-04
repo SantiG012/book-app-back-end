6aae004e42e139acf961c3de9b90ac321c484ab1
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-04 18:48:13 -0500

    Add the create author test

--- src/author/test/author.controller.spec.ts
+++ src/author/test/author.controller.spec.ts
@@ -2,11 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorController } from '../author.controller';
 import { AbstractAuthorService } from '../abstract-author.service';
 import { AuthorService } from '../author.service';
+import { AuthorIdDto, CreateAuthorDto } from '../author-dtos';
+import { createAuthorStub, sucessfulAuthorCreationStub } from './author.stubs';
 
 jest.mock('../author.service');
 
 describe('AuthorController', () => {
   let controller: AuthorController;
+  let authorService: AbstractAuthorService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,9 +21,31 @@ describe('AuthorController', () => {
     }).compile();
 
     controller = module.get<AuthorController>(AuthorController);
+    authorService = module.get<AbstractAuthorService>(AbstractAuthorService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create author', () => {
+    describe('when create author', () => {
+      let authorId:AuthorIdDto;
+
+      const createAuthorDto: CreateAuthorDto = createAuthorStub();
+
+      beforeEach(async () => {
+        authorId = await controller.createAuthor(createAuthorDto);
+      });
+
+      test('then it should call createAuthor', () => {
+        expect(authorService.createAuthor).toHaveBeenCalledWith(createAuthorDto);
+      });
+
+      test('then it should return authorId', () => {
+        expect(authorId).toEqual(sucessfulAuthorCreationStub());
+      });
+
+    });
   });
 });
