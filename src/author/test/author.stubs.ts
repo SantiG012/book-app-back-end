046625291e9ad51371bf08d9d8591c28cf023ccb
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-04 18:47:44 -0500

    Add the author creation stub

--- src/author/test/author.stubs.ts
+++ src/author/test/author.stubs.ts
@@ -1,7 +1,14 @@
-import { AuthorIdDto } from "../author-dtos";
+import { AuthorIdDto, CreateAuthorDto } from "../author-dtos";
 
 export const sucessfulAuthorCreationStub = ():AuthorIdDto => {
     return {
         authorId: "a1b2c3d4e5f6g7h8i9j0k1l2",
     };
+}
+
+export const createAuthorStub =():CreateAuthorDto =>{
+    return {
+        authorName: "John",
+        authorLastName: "Doe"
+    }
 }
