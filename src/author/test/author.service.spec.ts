c6f5edb078d54cdca42a312351e99322794ea40a
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-05 19:09:58 -0500

    Create the definition test

--- src/author/test/author.service.spec.ts
+++ src/author/test/author.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorService } from '../author.service';
+import { AbstractPrismaService } from '../../prisma/abstract-prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
+
+jest.mock('../../prisma/prisma.service');
 
 describe('AuthorService', () => {
   let service: AuthorService;
+  let prismaService: AbstractPrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthorService],
+      providers: [AuthorService,
+        {
+          provide: AbstractPrismaService,
+          useClass: PrismaService
+        }
+      ],
     }).compile();
 
     service = module.get<AuthorService>(AuthorService);
+    prismaService = module.get<AbstractPrismaService>(AbstractPrismaService);
+    
   });
 
   it('should be defined', () => {
