3abb646bf5724ff5c754daf000ad139cebf2c97f
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-07 18:41:23 -0500

    Add a new exception code

--- src/data-base-common-exceptions/exceptions-messages.ts
+++ src/data-base-common-exceptions/exceptions-messages.ts
@@ -4,5 +4,8 @@ export function createError(erroCode:string, resource:string):HttpException {
     if (erroCode === 'P2002') {
         return new HttpException(`${resource} already exists`, 409);
     }
+    if (erroCode === 'P2025') {
+        return new HttpException(`${resource} not found`, 404);
+    }
     return new HttpException('Internal server error', 500);
 } 
