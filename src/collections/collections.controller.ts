28aa90eaa8df7872db2e6220fafd55a7a33ec1f1
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-08 11:33:33 -0500

    Add the create collection method

--- src/collections/collections.controller.ts
+++ src/collections/collections.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CollectionIdDto, CreateCollectionDto } from './dtos/index';
+import { AbstractCollectionsService } from './abstract-collections.service';
 
 @Controller('collections')
-export class CollectionsController {}
+export class CollectionsController {
+    constructor(
+         private readonly collectionsService: AbstractCollectionsService
+    ) {}
+
+    @Post('createCollection')
+    async createCollection(@Body()createCollectionDto:CreateCollectionDto): Promise<CollectionIdDto> {
+        return await this.collectionsService.createCollection(createCollectionDto);
+    }
+}
