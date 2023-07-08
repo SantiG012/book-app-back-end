549d4f7d9404a4c65fa33265a4779cc6a2ab23a0
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-08 15:19:50 -0500

    Add the collection id stub

---
+++ src/collections/tests/stubs/collection-id.stub.ts
@@ -0,0 +1,9 @@
+import { CollectionIdDto } from "../../dtos/index";
+
+export const collectionIdStub = ():CollectionIdDto =>{
+    const collectionId:CollectionIdDto = {
+        collectionId:'collectionId'
+    }
+
+    return collectionId;
+}
