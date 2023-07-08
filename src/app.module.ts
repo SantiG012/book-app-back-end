072cb42b2bb76c3ed42542f866acdc434b5f2fcc
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-08 10:29:41 -0500

    Add the collections module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { BookModule } from './book/book.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthorModule } from './author/author.module';
+import { CollectionsModule } from './collections/collections.module';
 
 @Module({
-  imports: [AuthModule, BookModule, PrismaModule, AuthorModule],
+  imports: [AuthModule, BookModule, PrismaModule, AuthorModule, CollectionsModule],
   controllers: [],
   providers: []
 })
