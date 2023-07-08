497d9b80f1ff400bbd16d61da4094535f54cd38e
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-08 15:20:59 -0500

    Provide the mocked dependencies to the controller

--- src/collections/tests/collections.controller.spec.ts
+++ src/collections/tests/collections.controller.spec.ts
@@ -1,15 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CollectionsController } from '../collections.controller';
+import { AbstractCollectionsService } from '../abstract-collections.service';
+import { CollectionsService } from '../collections.service';
+
+jest.mock('../collections.service')
 
 describe('CollectionsController', () => {
   let controller: CollectionsController;
+  let collectionService: AbstractCollectionsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CollectionsController],
+      providers: [{
+        provide: AbstractCollectionsService,
+        useClass: CollectionsService
+      }]
     }).compile();
 
     controller = module.get<CollectionsController>(CollectionsController);
+    collectionService = module.get<AbstractCollectionsService>(AbstractCollectionsService);
   });
 
   it('should be defined', () => {
