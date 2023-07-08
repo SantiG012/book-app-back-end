ce8665f9aa36b9b230f0f71de4a5e449c0b7f3bc
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-08 11:32:10 -0500

    Add the create collection method

--- src/collections/collections.service.ts
+++ src/collections/collections.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { AbstractCollectionsService } from './abstract-collections.service';
+import { CreateCollectionDto, CollectionIdDto } from './dtos';
+import { AbstractPrismaService } from 'src/prisma/abstract-prisma.service';
+import { v4 as uuidv4 } from "uuid";
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { createError } from '../data-base-common-exceptions/exceptions-messages';
+import { create } from 'domain';
 
 @Injectable()
-export class CollectionsService {}
+export class CollectionsService implements AbstractCollectionsService {
+    constructor(
+        private readonly prismaService:AbstractPrismaService
+    ) {}
+
+    async createCollection(createCollectionDto: CreateCollectionDto): Promise<CollectionIdDto> {
+        const data = this.createCollectionObject(createCollectionDto);
+        let collection:CollectionIdDto;
+
+        try{
+             collection = await this.prismaService.collections.create({
+                    data: data,
+                    select: {
+                        collectionId: true
+                    }
+                });
+        }catch(error:PrismaClientKnownRequestError | any){
+            throw createError(error.code || 'UNKNOWN_ERROR','Collection');
+        }
+
+        return collection;
+    }
+
+    private createCollectionObject(createCollectionDto:CreateCollectionDto){
+        return {
+            collectionId: uuidv4(),
+            collectionName: createCollectionDto.collectionName,
+            userId: createCollectionDto.userId,
+            coverUrl: createCollectionDto.coverUrl,
+            collectionStatus:'active'
+        }
+    }
+
+}
