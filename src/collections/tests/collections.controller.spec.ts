9e72c56b57e4435cf211d0b03715da81072c401c
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-08 15:28:40 -0500

    Add the positive collection controller tests

--- src/collections/tests/collections.controller.spec.ts
+++ src/collections/tests/collections.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CollectionsController } from '../collections.controller';
 import { AbstractCollectionsService } from '../abstract-collections.service';
 import { CollectionsService } from '../collections.service';
+import { collectionIdStub, createCollectionStub } from './stubs';
+import { CollectionIdDto } from '../dtos';
 
 jest.mock('../collections.service')
 
@@ -24,5 +26,23 @@ describe('CollectionsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('createCollection', () => {
+    describe('when createCollection is called', () => {
+      let collectionId:CollectionIdDto;
+
+      beforeEach(async () => {
+        collectionId = await controller.createCollection(createCollectionStub());
+      });
+
+      test('then it should call collectionService', () => {
+        expect(collectionService.createCollection).toBeCalledWith(createCollectionStub());
+      });
+
+      test('then it should return a collectionId', () => {
+        expect(collectionId).toEqual(collectionIdStub());
+      });
+    });
   });
 });
