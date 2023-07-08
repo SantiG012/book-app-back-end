c239dedc5cce0849ae382b4917936bad21139c9d
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-08 11:17:27 -0500

    Add the AbstractCollectionService to the module

--- src/collections/collections.module.ts
+++ src/collections/collections.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CollectionsController } from './collections.controller';
 import { CollectionsService } from './collections.service';
+import { AbstractCollectionsService } from './abstract-collections.service';
 
 @Module({
   controllers: [CollectionsController],
-  providers: [CollectionsService]
+  providers: [{
+    provide: AbstractCollectionsService,
+    useClass: CollectionsService
+  }]
 })
 export class CollectionsModule {}
