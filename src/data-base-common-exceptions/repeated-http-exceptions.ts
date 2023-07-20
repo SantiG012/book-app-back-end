0db58bf21cd34793f53df746d7c990f194797514
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-20 13:39:15 -0500

    Add a conflict exception

--- src/data-base-common-exceptions/repeated-http-exceptions.ts
+++ src/data-base-common-exceptions/repeated-http-exceptions.ts
@@ -3,3 +3,7 @@ import { HttpException } from "@nestjs/common";
 export function badRequestException(resource:string):HttpException {
     return new HttpException(resource + ' already exists', 400);
 }
+
+export function conflictException(resource:string):HttpException {
+    return new HttpException(resource + ' already exists', 409);
+}
