09b63955fa812f1a088fe4f2f5be9ded7ecadcbf
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-21 18:46:02 -0500

    Mock the prismaService

--- src/collections/tests/collections.service.spec.ts
+++ src/collections/tests/collections.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CollectionsService } from '../collections.service';
+import { AbstractPrismaService } from '../../prisma/abstract-prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
+
+jest.mock('../../prisma/prisma.service');
 
 describe('CollectionsService', () => {
   let service: CollectionsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CollectionsService],
+      providers: [CollectionsService,{
+        provide: AbstractPrismaService,
+        useClass: PrismaService
+      }
+    ],
     }).compile();
 
     service = module.get<CollectionsService>(CollectionsService);
