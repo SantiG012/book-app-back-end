5163d5a29b278aa38887dd0155212400d3fdcf58
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-22 11:56:33 -0500

    Mock the create collection method

--- src/prisma/__mocks__/prisma.service.ts
+++ src/prisma/__mocks__/prisma.service.ts
@@ -1,7 +1,11 @@
+import { collectionIdStub } from "../../collections/tests/stubs";
 import { sucessfulBookCreationStub } from "../../book/test/stubs/index";
 
 export const PrismaService = jest.fn().mockReturnValue({
     book: {
         create: jest.fn().mockResolvedValue(sucessfulBookCreationStub())
+    },
+    collections: {
+        create: jest.fn().mockResolvedValue(collectionIdStub())
     }
 });
