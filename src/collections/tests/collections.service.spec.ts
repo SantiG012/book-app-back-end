09dc7f29272ef5f0a393e9cd58cf0a72f823ddc6
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-22 11:56:51 -0500

    Add the collection's succesful tests

--- src/collections/tests/collections.service.spec.ts
+++ src/collections/tests/collections.service.spec.ts
@@ -2,11 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CollectionsService } from '../collections.service';
 import { AbstractPrismaService } from '../../prisma/abstract-prisma.service';
 import { PrismaService } from '../../prisma/prisma.service';
+import { CollectionIdDto, CreateCollectionDto } from '../dtos';
+import { collectionIdStub, createCollectionStub } from './stubs';
 
 jest.mock('../../prisma/prisma.service');
 
 describe('CollectionsService', () => {
   let service: CollectionsService;
+  let prismaService: AbstractPrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,9 +21,43 @@ describe('CollectionsService', () => {
     }).compile();
 
     service = module.get<CollectionsService>(CollectionsService);
+    prismaService = module.get<AbstractPrismaService>(AbstractPrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create a collection', () => {
+    describe('when create a collection', () => {
+      let collectionId:CollectionIdDto;
+      const createCollectionDto:CreateCollectionDto = createCollectionStub();
+
+      beforeAll(async () => {
+        collectionId = await service.createCollection(createCollectionDto);
+      });
+
+      it('should return a collection id',()=>{
+        expect(collectionId).toEqual(collectionIdStub());
+      })
+
+      it('should call the prismaService',()=>{
+        expect(prismaService.collections.create).toBeCalledWith({
+          data: createCollectionDto,
+          select: {
+            collectionId: true
+          }
+        });
+      });
+
+      it('should call the prismaService only once',()=>{
+        expect(prismaService.collections.create).toBeCalledTimes(1);
+      });
+
+      it('should return a CollectionIdDto',()=>{
+        expect(collectionId).toStrictEqual(collectionIdStub());
+      });
+
+    });
   });
 });
