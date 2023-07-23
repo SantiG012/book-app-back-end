d5d052dabd60e4b379b358d618523885a180f19e
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-23 11:11:09 -0500

    Add the abstract service

---
+++ src/user/abstract-user.service.ts
@@ -0,0 +1,5 @@
+import { CreateUserDto } from "./user-dtos";
+
+export abstract class AbstractUserService {
+    abstract createUser(createUserDto:CreateUserDto): Promise<{ access_token: string }>;
+}
