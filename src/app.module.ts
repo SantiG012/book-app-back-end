62c8fa3dd20549305e9594528fb57fbb80d48338
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-25 13:11:24 -0500

    Import the config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthorModule } from './author/author.module';
 import { CollectionsModule } from './collections/collections.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, BookModule, PrismaModule, AuthorModule, CollectionsModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+    AuthModule, 
+    BookModule, 
+    PrismaModule, 
+    AuthorModule, 
+    CollectionsModule, 
+    UserModule
+  ],
   controllers: [],
   providers: []
 })
