6c5dc3c383c23dcca7905e5412dc4fbdf577b274
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-25 18:39:33 -0500

    Add the signUp method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AbstractAuthService } from './abstract-auth.service';
+import { CreateUserDto } from 'src/user/user-dtos';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService:AbstractAuthService
+    ) {}
+
+    @Post('signUp')
+    async signUp(@Body() createUserDto:CreateUserDto): Promise<{access_token:string}> {
+        return await this.authService.singUp(createUserDto);
+    }
+}
