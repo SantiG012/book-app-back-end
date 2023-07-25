666cfecfe6ca89b390968b959c5b285c1fdfb9ef
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-25 18:28:16 -0500

    Provide the abtract auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { AbstractAuthService } from './abstract-auth.service';
 
 @Module({
   imports: [
     UserModule,
     JwtModule.register({})
   ],
-  providers: [AuthService],
+  providers: [{
+    provide: AbstractAuthService,
+    useClass: AuthService
+  }],
   controllers: [AuthController]
 })
 export class AuthModule {}
