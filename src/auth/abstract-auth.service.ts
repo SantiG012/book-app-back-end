f1a3dfbcf80639d5b37c3534da721d2ebb1ca213
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-25 18:26:17 -0500

    Add the signUp method

--- src/auth/abstract-auth.service.ts
+++ src/auth/abstract-auth.service.ts
@@ -1,3 +1,5 @@
+import { CreateUserDto } from "src/user/user-dtos";
+
 export abstract class AbstractAuthService {
-    abstract singUp():Promise<{access_token:string}>;
+    abstract singUp(createUserDto:CreateUserDto):Promise<{access_token:string}>;
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { AbstractAuthService } from './abstract-auth.service';
+import { AbstractUserService } from 'src/user/abstract-user.service';
+import { CreateUserDto, UserCredentialsDto } from 'src/user/user-dtos';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService implements AbstractAuthService{
-    singUp(): Promise<{ access_token: string; }> {
-        throw new Error('Method not implemented.');
+    constructor(
+        private readonly userService: AbstractUserService,
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService
+    ) {}
+
+    async singUp(createUserDto:CreateUserDto): Promise<{ access_token: string; }> {
+        const userCredentials:UserCredentialsDto = await this.userService.createUser(createUserDto);
+
+        const payload = {
+            sub: userCredentials.userId,
+            userName: userCredentials.userName,
+            userLastName: userCredentials.userLastName,
+        };
+
+        const secret = this.configService.get<string>('JWT_SECRET');
+
+        const token = await this.jwtService.signAsync(
+            payload,
+            {
+                expiresIn: '45m',
+                secret: secret,
+            }
+        );
+
+        return { access_token: token };
     }
 }
