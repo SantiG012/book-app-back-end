b4588e9574fcc3988b3b30451f31e2c6de455c8b
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-26 17:19:48 -0500

    Add the jwt strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,38 @@
+import {ExtractJwt, Strategy} from 'passport-jwt';
+import {PassportStrategy} from '@nestjs/passport';
+import {Injectable} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly prismaService: PrismaService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false, 
+            secretOrKey: configService.get<string>('JWT_SECRET')
+        });
+    }
+
+    async validate(
+        payload: {
+            sub: string;
+            userName: string;
+            userLastName: string;
+        }
+    ):Promise<string> {
+        const {sub} = payload;
+
+        const user = await this.prismaService.user.findFirst({
+            where: {
+                userId: sub,
+                userStatus:'active'
+            }
+        });
+
+        return user.userId ? sub: null;
+    }
+}
