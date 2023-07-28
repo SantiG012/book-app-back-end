2aa6bc3b782c363c7b830b97a7d0891313b4afba
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-28 15:59:14 -0500

    Provide the JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,20 @@ import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AbstractAuthService } from './abstract-auth.service';
+import { JwtStrategy } from './strategy';
 
 @Module({
   imports: [
     UserModule,
     JwtModule.register({})
   ],
-  providers: [{
-    provide: AbstractAuthService,
-    useClass: AuthService
-  }],
+  providers: [
+      {
+        provide: AbstractAuthService,
+        useClass: AuthService
+      },
+      JwtStrategy
+  ],
   controllers: [AuthController]
 })
 export class AuthModule {}
