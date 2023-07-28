3df58a5c5e2d0520b3cb9c8335ac2d8cb7138d3a
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-28 15:46:11 -0500

    Add the sign in method

--- src/auth/abstract-auth.service.ts
+++ src/auth/abstract-auth.service.ts
@@ -1,5 +1,6 @@
-import { CreateUserDto } from "src/user/user-dtos";
+import { CreateUserDto, LogInDto } from "src/user/user-dtos";
 
 export abstract class AbstractAuthService {
     abstract singUp(createUserDto:CreateUserDto):Promise<{access_token:string}>;
+    abstract signIn(logInDto:LogInDto):Promise<{access_token:string}>;
 }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AbstractAuthService } from './abstract-auth.service';
-import { CreateUserDto } from 'src/user/user-dtos';
+import { CreateUserDto, LogInDto } from 'src/user/user-dtos';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +11,10 @@ export class AuthController {
     @Post('signUp')
     async signUp(@Body() createUserDto:CreateUserDto): Promise<{access_token:string}> {
         return await this.authService.singUp(createUserDto);
+    }
+
+    @Post('signIn')
+    async signIn(@Body() logInDto:LogInDto): Promise<{access_token:string}> {
+        return await this.authService.signIn(logInDto);
     }
 }
