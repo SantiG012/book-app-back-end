baefef916966d5fb32c03e39401484745fdc1fc2
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-28 15:43:21 -0500

    Add the sig in method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AbstractAuthService } from './abstract-auth.service';
 import { AbstractUserService } from 'src/user/abstract-user.service';
-import { CreateUserDto, UserCredentialsDto } from 'src/user/user-dtos';
+import { CreateUserDto, LogInDto, UserCredentialsDto, UserIdDto } from 'src/user/user-dtos';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { User } from '@prisma/client';
+import * as argon from 'argon2';
+import { Payload } from './dtos';
 
 @Injectable()
 export class AuthService implements AbstractAuthService{
@@ -31,11 +34,37 @@ export class AuthService implements AbstractAuthService{
         return { access_token: token };
     }
 
-    private createPayload(userCredentials:UserCredentialsDto){
+    async signIn(logInDto:LogInDto): Promise<{ access_token: string; }> {
+        const userIdDto:UserIdDto = { userId: logInDto.userId };
+
+        const user:User = await this.userService.getUser(userIdDto);
+
+        const isPasswordValid = await argon.verify(user.password, logInDto.password);
+
+        if(!isPasswordValid){
+            throw new ForbiddenException('Invalid credentials');
+        }
+
+        const token:string = await this.generateToken(this.createPayload(user));
+
+        return { access_token: token };
+    }
+
+    private createPayload(user:{userId:string, userName:string, userLastName:string}): Payload {
         return {
-            sub: userCredentials.userId,
-            userName: userCredentials.userName,
-            userLastName: userCredentials.userLastName,
+            sub: user.userId,
+            userName: user.userName,
+            userLastName: user.userLastName,
         };
+    }
+
+    private async generateToken(payload: Payload): Promise<string> {
+        return await this.jwtService.signAsync(
+            payload,
+            {
+                expiresIn: '45m',
+                secret: this.configService.get<string>('JWT_SECRET'),
+            }
+        );
     }
 }
