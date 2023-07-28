8a5c8708a8a04904c3fd7ef56079bf40f0e06c2d
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-07-28 15:59:31 -0500

    Deleted unused services

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,14 +2,12 @@ import {ExtractJwt, Strategy} from 'passport-jwt';
 import {PassportStrategy} from '@nestjs/passport';
 import {Injectable} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { Payload } from '../dtos';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy,'jwt') {
     constructor(
         private readonly configService: ConfigService,
-        private readonly prismaService: PrismaService
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
