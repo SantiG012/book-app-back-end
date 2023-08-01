eb9215529c8211b3200cd9b0b2fa18d957d6adce
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-08-01 14:07:09 -0500

    Add the collectio info dto

---
+++ src/collections/dtos/collection-info.dto.ts
@@ -0,0 +1,5 @@
+export interface CollectionInfoDto {
+    collectionId: string;
+    collectionName: string;
+    coverUrl: string;
+}
--- src/collections/dtos/index.ts
+++ src/collections/dtos/index.ts
@@ -1,3 +1,4 @@
 export * from './collection-id.dto';
 export * from './create-collection.dto';
 export * from './add-books.dto';
+export * from './collection-info.dto';
