35dd17e6203878a669942b5e79d342f3252f96b5
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-08-02 10:29:45 -0500

    Add the getBooksByCollectionId endpoint

--- src/collections/collections.controller.ts
+++ src/collections/collections.controller.ts
@@ -1,16 +1,18 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { CollectionBookDto, CollectionIdDto, CollectionInfoDto, CreateCollectionDto } from './dtos/index';
 import { AbstractCollectionsService } from './abstract-collections.service';
 import { JwtAuthGuard } from 'src/auth/guards';
 import { GetUser } from 'src/auth/decorators';
 import { CountDto } from 'src/global-dtos';
 import { BookInfoDto } from 'src/book/book-dtos';
+import { AbstracBookService } from 'src/book/abstract-book.service';
 
 @UseGuards(JwtAuthGuard)
 @Controller('collections')
 export class CollectionsController {
     constructor(
-         private readonly collectionsService: AbstractCollectionsService
+         private readonly collectionsService: AbstractCollectionsService,
+         private readonly bookService:AbstracBookService
     ) {}
 
     @Post('createCollection')
@@ -29,5 +31,13 @@ export class CollectionsController {
     @Get('getCollections')
     async getCollections(@GetUser('sub') sub: string): Promise<CollectionInfoDto[]> {
         return await this.collectionsService.getCollections(sub);
+    }
+
+    @Get('getBooksByCollectionId/:collectionId')
+    async getBooksByCollectionId(@Param('collectionId')collectionId:CollectionIdDto):Promise<BookInfoDto[]>{
+        const bookIds:string[] = await this.collectionsService.getBooksByCollectionId(collectionId);
+        const books:BookInfoDto[] = await this.bookService.findBooksById(bookIds);
+        
+        return books;
     }
 }
--- src/collections/collections.module.ts
+++ src/collections/collections.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CollectionsController } from './collections.controller';
 import { CollectionsService } from './collections.service';
 import { AbstractCollectionsService } from './abstract-collections.service';
+import { BookModule } from 'src/book/book.module';
 
 @Module({
+  imports: [BookModule],
   controllers: [CollectionsController],
   providers: [{
     provide: AbstractCollectionsService,
