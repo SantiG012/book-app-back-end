2f24d40f942c1befebe7020da90be9d9a71c518b
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-08-03 09:39:08 -0500

    Add the collection name dto

--- src/collections/dtos/index.ts
+++ src/collections/dtos/index.ts
@@ -2,3 +2,4 @@ export * from './collection-id.dto';
 export * from './create-collection.dto';
 export * from './collection-book.dto';
 export * from './collection-info.dto';
+export * from './collection-name.dto'
